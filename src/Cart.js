91d4f8da87e9401d5c0d498e83067f336c542281
Harish Jadhav <harishjadhav675@gmail.com>
2023-08-29 22:52:00 +0530

    creating the probs

--- src/Cart.js
+++ src/Cart.js
@@ -1,16 +1,51 @@
 import React from "react";
 import CartItem from "./CartItem";
 class Cart extends React.Component{
+     constructor(){
+          super();
+         this.state = {       
+             products: [
+                { 
+                    price:80,
+                    title:'Watch',
+                    qty:1,
+                    img:'',
+                    id:1,
+
+                }  ,
+                { 
+                    price:9999,
+                    title:'mobilePhone',
+                    qty:2,
+                    img:'',
+                    id:2,
+
+                }  ,
+                { 
+                    price:1999,
+                    title:'Phone',
+                    qty:7,
+                    img:'',
+                    id:3,
+
+                }  
+             ]
+         }
+             // this.increaseQuantity = this.increaseQuantity.bind(this);         
+     }
       
      render(){
+   
+         const {products} = this.state;         
          
           return(
              
-             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+             <div className="cart">  
+                 {products.map((product)=>{
+                       
+                 return <CartItem product={product}  key={product.id} /> 
+                       
+                 })}
              </div>
           )
      }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 class CartItem extends React.Component{
-     
-     constructor(){
-          super();
-         this.state = {       
-             price:999,
-             title:'Phone',
-             qty:1,
-             img:''
-         }
-             // this.increaseQuantity = this.increaseQuantity.bind(this);         
-     }
+    
 
      increaseQuantity = ()=>{
        
@@ -62,7 +52,8 @@ class CartItem extends React.Component{
     }
 
     render(){
-        const {price ,title,qty} = this.state
+        console.log(this.props);
+        const {price ,title,qty} = this.props.product;
          return(
             <div className="card-item">
               
