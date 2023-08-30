d091432c7365f4464102c6af7c4e2c9d19f39797
Harish Jadhav <harishjadhav675@gmail.com>
2023-08-30 10:33:51 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CartItem from "./CartItem";
 import Cart  from "./Cart";
+import Navbar from './Navbar';
 
 
 function App() {
   return (
     <div className="App">
+       <Navbar/>
        <Cart/>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+
+class Navbar extends React.Component{
+     
+ render(){
+     
+      return (
+           
+         <div>
+             <div>
+                 <img src="" alt="cart-icon" />
+             </div>
+             <span>3</span>
+
+         </div>
+         
+      )
+ }
+
+
+
+}
+
+export default Navbar;
