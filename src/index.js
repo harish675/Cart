f4793280178f78c6154bf9016636c3bb5237f13c
Harish Jadhav <harishjadhav675@gmail.com>
2023-09-03 19:35:41 +0530

    adding firebase to cart

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA-20jeyT5J4Ro0YnAlmimzO13Dh0JBDLI",
+  authDomain: "cart-20cb2.firebaseapp.com",
+  projectId: "cart-20cb2",
+  storageBucket: "cart-20cb2.appspot.com",
+  messagingSenderId: "345713707108",
+  appId: "1:345713707108:web:5ba61950922b388266f771"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
